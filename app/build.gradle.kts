c627165f6560b13e6e10973fc38abba3996963bb
eric <26416387.re@gmail.com>
2026-04-06 23:00:06 +0900

    yolo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "com.hualien.taxidriver"
         minSdk = 26
         targetSdk = 36
-        versionCode = 12
-        versionName = "beta11"
+        versionCode = 13
+        versionName = "beta13"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
